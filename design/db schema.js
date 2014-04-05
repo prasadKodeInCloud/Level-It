72a77993d72da259e99aa0b80db1d4702b5e2a50
Manujith Pallewatte <manujith.nc@gmail.com>
2014-04-05 18:31:29 +0530

    adding schema

---
+++ design/db schema.js
@@ -0,0 +1,28 @@
+
+//Entity Types
+//========
+{ _id:"RECTANGLE", isResizable:false, imageUrl:"../url", styles:{fill:'#E9F1FA', stroke:'#DBE2F0',strokeWidth: 2,opacity: 1,isDraggable:true,isLocked:true,xCells:2, yCells:1}}
+{ _id:"RESIZABLE_RECTANGLE", isResizable:true }
+{ _id:"CIRCLE", isResizable:false }
+{ _id:"RESIZABLE_CIRCLE", isResizable:true }
+
+
+//Users
+//=====
+{_id:"autoid", name:"asakjs"}
+
+//Projects
+//========
+{_id:"autoid", name:"name", userID:"userID"}
+
+//Levels
+//======
+{ id:"autoid", name:"level1", projectID:"projectID",settings:{width:800, height:480, orientation:"LANDSCAPE"}}
+
+//Entity
+//======
+{_id:"autoid", entityTypeId:"RECTANGLE", styles:{rowIndex: 1, colIndex:3, xCells:2, yCells:1, angle:30}}
+
+
+
+
